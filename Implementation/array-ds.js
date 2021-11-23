567c21b728ac62f61244b41edc122e244c00b651
LENINDALLAS <lenindallas.ld@gmail.com>
2021-11-23 21:09:25 +0530

    updates

---
+++ Implementation/array-ds.js
@@ -0,0 +1,13 @@
+/*
+ * Complete the 'reverseArray' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts INTEGER_ARRAY a as parameter.
+ */
+
+function reverseArray(a) {
+    // Write your code here
+    const reversed = [];
+    a.forEach(item => reversed.unshift(item));
+    return (reversed)
+}
