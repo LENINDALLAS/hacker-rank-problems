aac60792e2641b7332563fb5a21e0686690d1d34
LENINDALLAS <lenindallas.ld@gmail.com>
2022-01-29 23:22:44 +0530

    adding optimized version sort two linked list

--- dsa/mergeLinkedList.js
+++ dsa/mergeLinkedList.js
@@ -45,3 +45,54 @@ function mergeLists(head1, head2) {
     }
     return (res)
 };
+
+//optimized version of sorting two linked list nodes
+
+/*
+ * For your reference:
+ *
+ * SinglyLinkedListNode {
+ *     int data;
+ *     SinglyLinkedListNode next;
+ * }
+ *
+ */
+function mergeLists(head1, head2) {
+    let list1 = head1,
+        list2 = head2,
+        list,
+        acc;
+
+    function Node(value) {
+        this.data = value;
+        this.next = null;
+    }
+
+    while (list1 !== null || list2 !== null) {
+        let list1data = list1 === null ? -1 : list1.data,
+            list2data = list2 === null ? -1 : list2.data;
+
+        if (list1data >= list2data && list2data !== -1
+            || list1data === -1) {
+            if (!list) {
+                list = new Node(list2data);
+                acc = list;
+            } else {
+                acc.next = new Node(list2data);
+                acc = acc.next;
+            }
+            list2 = list2.next;
+        } else {
+            if (!list) {
+                list = new Node(list1data);
+                acc = list;
+            } else {
+                acc.next = new Node(list1data);
+                acc = acc.next;
+            }
+            list1 = list1.next;
+        }
+    }
+
+    return list;
+};
